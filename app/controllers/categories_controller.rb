de3f941d79ebcc89237240cdb04d8f8c36087c5c
Sara Al-Jammal <sara.aljammal97@gmail.com>
2020-09-14 11:51:15 +0300

    added categories, was not resolved

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,55 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, :except => [:index, :new, :create]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+    authorize @category
+  end
+
+  def create
+    @category = Category.new()
+    authorize @category
+    if @category.save
+      redirect_to categories_path, alert: "Category #{@category.name} has been created"
+    else
+      render new_category_path
+    end
+  end
+
+  def show
+    authorize @category
+    respond_to do |format|
+      format.html {render :show }
+    end
+  end
+
+  def edit
+    authorize @category
+  end
+
+  def update
+    @category.update(category_params)
+    redirect_to category_path, alert: "This category has been updated"
+  end
+
+  def destroy
+    authorize @category
+    @category.destroy
+    redirect_to root_path, alert: "Category #{@category.name} has been deleted"
+  end
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
+
+
